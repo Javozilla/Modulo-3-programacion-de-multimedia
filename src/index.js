130b80541d13e7f5a436e41611d5ce2fd985cb06
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-11-20 16:53:48 -0600

    changing inddex

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Landing_page />
     </BrowserRouter>
   </React.StrictMode>
 );
