e7974906ec188a4e5aae47b6b73c49ad4a3c5e2b
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-11-20 15:17:00 -0600

    updateing

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import {
 } from './Containers';
 
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-      <Landing_page />
-      <AboutUsPage />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
