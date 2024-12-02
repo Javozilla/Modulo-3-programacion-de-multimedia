f415362f920721b7281f15f6e597756cce7cbfb5
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-12-02 16:34:59 -0600

    fuckkkkkkk

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Containers/LandingPage/App';
 import AboutUsPage from './AboutUsPage/AboutUsPage';
+import CartPage from './cartPage/cartIndex';
+import ProductsPage from './ProductsPage/products';
+import App from './Containers/LandingPage/App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductsPage from './ProductsPage/products';
 
@@ -12,8 +15,10 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/AboutUsPage/AboutUsPage" element={<AboutUsPage />} />
-        <Route path="/ProductsPage/products" element={<ProductsPage />} />
+        <Route path="/AboutUsPage/AboutUsPage.js" element={<AboutUsPage />} />
+        <Route path="/ProductsPage/products.js" element={<ProductsPage />} />
+        <Route path="/cartPage/cartIndex.js" element={<CartPage />} />
+        <Route path="/LandingPage/App.js" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
