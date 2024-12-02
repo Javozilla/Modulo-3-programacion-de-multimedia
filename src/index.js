bbd81e510461209340d07cd1563ac2c2756db685
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-12-02 16:48:14 -0600

    waaaaa6a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Containers/LandingPage/App';
+import App from './Containers/LandingPage/App.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductsPage from './ProductsPage/products';
-import CartPage from './cartPage/cartIndex';
-import AboutUsPage from './AboutUsPage/AboutUsPage';
+import ProductsPage from './ProductsPage/products.js';
+import CartPage from './cartPage/cartIndex.js';
+import AboutUsPage from './AboutUsPage/AboutUsPage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,10 +13,10 @@ root.render(
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/LandingPage/App" element={<App />} />
-        <Route path="/AboutUsPage/AboutUsPage" element={<AboutUsPage />} />
-        <Route path="/ProductsPage/products" element={<ProductsPage />} />
-        <Route path="/cartPage/cartIndex" element={<CartPage />} />
+        <Route path="/LandingPage/App.js" element={<App />} />
+        <Route path="/AboutUsPage/AboutUsPage.js" element={<AboutUsPage />} />
+        <Route path="/ProductsPage/products.js" element={<ProductsPage />} />
+        <Route path="/cartPage/cartIndex.js" element={<CartPage />} />
       </Routes>
     </Router>
   </React.StrictMode>
