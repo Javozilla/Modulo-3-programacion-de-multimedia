04590b561e7c2e04379ddb1db10230bc30bf29a3
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-12-03 15:55:01 -0600

    something something

--- src/Containers/index.js
+++ src/Containers/index.js
@@ -1,3 +1,5 @@
 
 export {default as AboutUsPage} from './AboutUsPage/AboutUsPage';
 export {default as Landing_page} from './LandingPage/App';
+export {default as ProductsPage} from './ProductsPage/ProductsPage';
+export {default as CartPage} from './CartPage/CartPage';
