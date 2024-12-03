f75dc910890966f876f9241142f5f84effd5c1b6
Javier Humberto Gonzalez <A01253836@itesm.mx>
2024-12-02 18:08:01 -0600

    wakjshd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Routes as RouterRoutes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes as RouterRoutes, Route } from 'react-router-dom';
 import Routes from './routes';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import App from './Containers/LandingPage/App';
 import AboutUsPage from './Containers/AboutUsPage/AboutUsPage';
-import ProductsPage from './ProductsPage/products';
-import CartPage from './cartPage/cartIndex';
+import ProductsPage from './Containers/ProductsPage/products';
+import CartPage from './Containers/cartPage/cartIndex';
 
 const Routes = () => {
   return (
     <>
         <Route path="/" element={<App />} />
-        <Route path="/Containers/LandingPage/App.js" element={<App />} />
-        <Route path="/Containers/AboutUsPage/AboutUsPage.js" element={<AboutUsPage />} />
-        <Route path="/Containers/ProductsPage/products.js" element={<ProductsPage />} />
-        <Route path="/Containers/cartPage/cartIndex.js" element={<CartPage />} />
+        <Route path="/about" element={<AboutUsPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
     </>
   );
 };
